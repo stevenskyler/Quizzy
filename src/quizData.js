c5e770a1524a7216c86924168ae5778ea1fe1ca9
stevenskyler <skylersandeep001@gmail.com>
2024-08-07 00:38:56 +0530

    Initial commit

---
+++ src/quizData.js
@@ -0,0 +1,30 @@
+const quizData = [
+    {
+      question: "Who is the best pc-gamer among these?",
+      options: ["Saumya!", "Sudarshan", "Rupesh", "Saurav"],
+      answer: "Saumya!"
+    },
+    {
+      question: "Who is the best chess player among these?",
+      options: ["Saumya", "Sudarshan", "Rupesh", "Supriya!"],
+      answer: "Supriya!"
+    },
+    {
+      question: "Who is the best dancer in our team?",
+      options: ["Saumya", "Nirav", "Rupesh", "Dhruvi"],
+      answer: "Dhruvi"
+    },
+    {
+      question: "What is the most stylish person in our team?",
+      options: ["Supriya", "Apurva", "Sudarshan", "Dhruvi"],
+      answer: "Sudarshan"
+    },
+    {
+      question: "Who is the most intelligent,smart,kind,stylish and every quality having person in our team?",
+      options: ["Sandeep sir", "Pintu", "Steven Skyler", "Sandeep Thakur"],
+      answer: "Pintu"
+    },
+  ];
+  
+  export default quizData;
+  
